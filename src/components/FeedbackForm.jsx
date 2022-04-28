ad3fe10e1b29a60e3f4da06739df551c5aba7ffa
carlosg <carlos.gutierrez.873@gmail.com>
2022-04-28 01:54:04 -0700

    Added submission form with validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,6 +1,47 @@
+import { useState } from "react"
+import Card from "./shared/Card"
+import Button from "./shared/Button"
+
 function FeedbackForm() {
+    const [text,setText] = useState('')
+    const [btnDisabled,setbtnDisabled] = useState(true)
+    const [message,setMessage] = useState('')
+
+    const handleTextChange = (e) => {
+        if(text === ''){
+            setbtnDisabled(true)
+            setMessage(null)
+        } else if(text !== '' && text.trim().length <= 10){
+            setbtnDisabled(true)
+            setMessage('Text must be at least 10 characters')
+        } else {
+            setMessage(null)
+            setbtnDisabled(false)
+        }
+        
+        setText(e.target.value)
+    }
+
   return (
-    <div>FeedbackForm</div>
+    <Card>
+        <form>
+            <h2>How would you rate your service with us?</h2>
+            {}
+            <div className="input-group">
+                <input 
+                    onChange={handleTextChange} 
+                    type='text'
+                    placeholder='Write a review' 
+                    value={text}
+                />
+                <Button type="submit" version='primary' isDisabled={btnDisabled}>
+                    Send
+                </Button>
+            </div>
+
+            {message && <div className='message'>{message}</div>}
+        </form>
+    </Card>
   )
 }
 
