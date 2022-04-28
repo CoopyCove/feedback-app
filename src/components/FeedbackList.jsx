faf67f4eee44a201926a71ff552e9a0f6523d02d
carlosg <carlos.gutierrez.873@gmail.com>
2022-04-28 00:49:54 -0700

    added a delete to each feedback bubble as well as score average

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
 
-function FeedbackList({feedback}) {
+function FeedbackList({feedback, handleDelete}) {
     if(!feedback || feedback.length === 0){
         return <p>No Feedback Yet</p>
     }
@@ -9,14 +9,16 @@ function FeedbackList({feedback}) {
   return (
     <div className='feedback-List'>
         {feedback.map((item) => (
-            <FeedbackItem key={item.id} item={item} />
+            <FeedbackItem key={item.id} 
+            item={item} 
+            handleDelete={handleDelete}/>
         ))}
     </div>
   )
 }
 
 FeedbackList.propTypes = {
-    feedback: PropTypes.arrarOf(
+    feedback: PropTypes.arrayOf(
         PropTypes.shape({
             id: PropTypes.number.isRequired,
             text: PropTypes.string.isRequired,
