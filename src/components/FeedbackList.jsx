742ff0e83700566ef42d0c078e07eb4cef40402a
carlosg <carlos.gutierrez.873@gmail.com>
2022-04-27 21:49:02 -0700

    Proptypes set up and card component was made

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+import FeedbackItem from './FeedbackItem'
+
+function FeedbackList({feedback}) {
+    if(!feedback || feedback.length === 0){
+        return <p>No Feedback Yet</p>
+    }
+
+  return (
+    <div className='feedback-List'>
+        {feedback.map((item) => (
+            <FeedbackItem key={item.id} item={item} />
+        ))}
+    </div>
+  )
+}
+
+FeedbackList.propTypes = {
+    feedback: PropTypes.arrarOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+            rating: PropTypes.number.isRequired,
+        })
+    ),
+  }
+
+export default FeedbackList
