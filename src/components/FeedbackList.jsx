20851d388820cbb43066ff4c3f65dde90ee1e9d9
carlosg <carlos.gutierrez.873@gmail.com>
2022-04-28 03:21:22 -0700

    The feedback section of the app now works with animations.

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import {motion, AnimatePresence} from 'framer-motion'
 import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
 
@@ -8,11 +9,20 @@ function FeedbackList({feedback, handleDelete}) {
 
   return (
     <div className='feedback-List'>
+      <AnimatePresence>
         {feedback.map((item) => (
+          <motion.div 
+          key={item.id}
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          exit={{opacity: 0}}
+          >
             <FeedbackItem key={item.id} 
             item={item} 
             handleDelete={handleDelete}/>
+            </motion.div>
         ))}
+        </AnimatePresence>
     </div>
   )
 }
