1be5453e5837f4889648192fb124feacfe38af1a
carlosg <carlos.gutierrez.873@gmail.com>
2022-04-28 19:17:16 -0700

    added edit function to feedback

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -9,13 +9,36 @@ export const FeedbackProvider = ({children}) => {
             id: 1,
             text: 'This is Feedback item 1',
             rating: 10
-        }
+        },
+        {
+            id: 2,
+            text: 'This is Feedback item 2',
+            rating: 6
+        },
     ])
+    const [feedbackEdit, setFeedbackEdit] = useState({
+        item: {},
+        edit: false
+    })
 
     const deleteFeedback = (id) => {
         if(window.confirm('Are you sure you want to delete')){
         setFeedback(feedback.filter((item) => item.id !== id))
         }
+    }
+
+    const updateFeedback = (id, updItem) => {
+        setFeedback(
+        feedback.map((item) => (item.id === id ? { ...item,
+        ...updItem } : item))
+        )
+    }
+
+    const editFeedback = (item) => {
+        setFeedbackEdit({
+            item, 
+            edit: true
+        })
     }
 
     const addFeedback = (newFeedback) => {
@@ -29,6 +52,9 @@ export const FeedbackProvider = ({children}) => {
             feedback,
             deleteFeedback,
             addFeedback,
+            editFeedback,
+            feedbackEdit,
+            updateFeedback
         }}
     >
         {children}
